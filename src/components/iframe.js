eb2dfb75ff0c122dd27ce28a338f97dcf16a81f4
Tak Tran <contact@tutaktran.com>
2018-09-25 12:11:06 +0100

    Add content constants

--- src/components/iframe.js
+++ src/components/iframe.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe';
+import { ARTICLES } from '../content';
 
 export default class PageIframe extends Component {
     render() {
     return (  
-    <Iframe url="https://ft.com"
+    <Iframe url={ARTICLES.parent}
     width="100%"
     height="100%"
     id="iframe"
---
+++ src/content.js
@@ -0,0 +1,12 @@
+export const ARTICLES = {
+  // Vegan food & drink articles
+  parent: 'https://www.ft.com/content/81edaaec-bced-11e8-94b2-17176fbf93f5',
+  children: [
+    'https://www.ft.com/content/a529c6f6-bba0-11e8-8274-55b72926558f',
+    'https://www.ft.com/content/df4a8600-bb9f-11e8-8274-55b72926558f',
+    'https://www.ft.com/content/ed2ac0cc-bba5-11e8-8274-55b72926558f',
+    'https://www.ft.com/content/3b210ddc-bba0-11e8-8274-55b72926558f',
+    'https://www.ft.com/content/c6f9ade0-bba1-11e8-8274-55b72926558f',
+    'https://www.ft.com/content/a7cd2eae-bb9e-11e8-8274-55b72926558f'
+  ]
+}
